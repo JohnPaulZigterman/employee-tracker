a89bf366390fe6abf6608e414db58e11b32a135b
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-10-28 09:37:10 -0400

    fixed schema and seed formatting and content issues

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
+  salary DECIMAL(5,2),
   department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
@@ -23,6 +23,6 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT,
+  manager_id INT
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,10 @@
-INSERT INTO movies (movie_name)
-VALUES ("Lock, Stock, and Two Smoking Barrels"),
-       ("Sphere"),
-       ("Killers Of The Flower Moon"),
-       ("Mystery Men"),
-       ("Rat Race");
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("IT"),
+       ("Accounting"),
+       ("HR"),
+       ("Production");
 
-INSERT INTO reviews (movie_id, review)
-VALUES (2, "It stinks! They just forget at the end."),
-       (3, "Pretty good for Marty."),
-       (4, "The best movie of all time"),
-       (4, "Undeniably Good"),
-       (4, "Wow! Incredible!"),
-       (5, "Also Very GOOD"),
-       (1, "Guy Ritchie at his best"),
-       (1, "TOO MANY WORDS");
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 100.55, 1),
+       ('Sales Associate', 70.55, 1);
