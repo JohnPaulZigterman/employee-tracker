7a4315d3ba53fba1809e8d5a66fdfb2e088dc0f2
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-10-28 08:45:06 -0400

    added initial files

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO movies (movie_name)
+VALUES ("Lock, Stock, and Two Smoking Barrels"),
+       ("Sphere"),
+       ("Killers Of The Flower Moon"),
+       ("Mystery Men"),
+       ("Rat Race");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (2, "It stinks! They just forget at the end."),
+       (3, "Pretty good for Marty."),
+       (4, "The best movie of all time"),
+       (4, "Undeniably Good"),
+       (4, "Wow! Incredible!"),
+       (5, "Also Very GOOD"),
+       (1, "Guy Ritchie at his best"),
+       (1, "TOO MANY WORDS");
---
+++ server.js
@@ -0,0 +1,3 @@
+//imports express and mysql
+const express = require('express');
+const mysql = require('mysql2');
