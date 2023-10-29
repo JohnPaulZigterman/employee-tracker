22a9c18f3a34de32691b12e8e4156433709e879d
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-10-29 11:56:56 -0400

    corrected schema and seed files

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,16 +6,16 @@ VALUES ("Sales"),
        ("Production");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Sales Lead', 100.55, 1),
-       ('Sales Associate', 70.55, 1),
-       ('Head of IT', 120.55, 2),
-       ('IT Associate', 80.55, 2),
-       ('Head Accountant', 150.55, 3),
-       ('Accountant', 100.33, 3),
-       ('HR Manager', 125.44, 4),
-       ('HR Intern', 40.81, 4),
-       ('Production Manager', 110.58, 5),
-       ('Production Associate', 70.65, 5);
+VALUES ('Sales Lead', 10055, 1),
+       ('Sales Associate', 70550, 1),
+       ('Head of IT', 12055, 2),
+       ('IT Associate', 80550, 2),
+       ('Head Accountant', 15055, 3),
+       ('Accountant', 10033, 3),
+       ('HR Manager', 12544, 4),
+       ('HR Intern', 40810, 4),
+       ('Production Manager', 11580, 5),
+       ('Production Associate', 70650, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Lemmy', "Killminster", 1, null),
